7c7a07c08dd27216648f06b802f2bf547a61368a
Frost199 <emmaldini12@gmail.com>
2019-11-12 08:33:45 +0100

    Refactor unit test for database connection to fail

--- spec/unit/app-unit-spec.js
+++ spec/unit/app-unit-spec.js
@@ -4,11 +4,11 @@ describe('PostgreSQL failed connection', () => {
   //Connecting to database
   let conString = '1234';
   let client = new pg.Client(conString);
-  let err = new Error(`could not connect to postgres:`);
+  let errOut = new Error(`could not connect to postgres`);
 
   it('should fail connecting', function () {
     client.connect((err) => {
-      expect(err).toThrow();
+      expect(err).toEqual(errOut);
     });
   });
 });
